f430e918c7ab3833f9ba2761d2abc3607205612c
Ana Beatriz Custódio <anabeatriz.custodio.oliveira@gmail.com>
2025-07-07 12:16:05 -0300

    Update dataTypesObject.js
    explicação de objetos

--- APRENDENDO-JAVASCRIPT/Aula03-Tipos-de-dados/dataTypesObject.js
+++ APRENDENDO-JAVASCRIPT/Aula03-Tipos-de-dados/dataTypesObject.js
@@ -1,3 +1,5 @@
+// {} = objeto vazio, itens dentro do objeto são chamados de propriedades; e o que está fora é a variável, ex.: const fichaDaBia
+// toda propriedade precisa ser chamada, seguida por dois pontos e depois o valor, não esquecer de separar por virgula
 
 const fichaDaBia = { 
   nome: "Beatriz", 
@@ -24,6 +26,3 @@ console.log('Ficha da Bia', fichaDaBia);
 // };
 
 // console.log('Ficha da Bia', fichaDaBia);
-
-
-
