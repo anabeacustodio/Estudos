7187afe2c42b94fa757211748a5d7b29b84a32f9
Ana Beatriz Custódio <anabeatriz.custodio.oliveira@gmail.com>
2025-09-03 15:44:49 -0300

    Delete curso-python directory duplicated

--- curso-python/1-hello.py
+++
@@ -1,4 +0,0 @@
-# Palavra reservada (sintaxe com função):
-
-print("Hello, Word!")
-# printa na tela o que esta entre parenteses, nesse caso por ser uma string, utilizamos aspas
--- curso-python/3-operadores.py
+++
@@ -1,9 +0,0 @@
-# Operadore Matemáticos
-
-# Operações básicas: +, -, *, /, %, **
-# % -> resto da divisão! Ex.: 4 % 2 tem resto 0
-# ** -> elevado a, 4 elevado a 2 (potência)
-
-# Operadores de compareção: ==, !=, >, <, >=, <=
-
-print(2 + 2)
