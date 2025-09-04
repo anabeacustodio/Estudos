67c62d7a9c00a08234720de9b4f18f5bdefa9f98
Ana Beatriz Custódio <anabeatriz.custodio.oliveira@gmail.com>
2025-09-04 10:02:05 -0300

    Atualizando versão local mais recente Atualizando versão local mais recente ''

--- Python/Aula01-hello.py
+++ Python/Aula01-hello.py
@@ -1,4 +1,7 @@
+# Abrir o terminal e executar o comando: python3 Aula01-hello.py
+# para rodar o arquivo python
+
 # Palavra reservada (sintaxe com função):
 
-print("Hello, Word!")
+print("Hello, World!")
 # printa na tela o que esta entre parenteses, nesse caso por ser uma string, utilizamos aspas
